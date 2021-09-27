ecc0fb6c5df3e80898fdc4e9fe8e8e1f42469600
nikolaf99 <49831725+nikolaf99@users.noreply.github.com>
2021-09-26 20:37:23 -0700

    Update colors.ts

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -2,7 +2,7 @@ import { Colors } from "./types";
 
 export const baseColors = {
   failure: "#ED4B9E",
-  primary: "#aa8929",
+  primary: "##08bc8f",
   primaryBright: "#aa8929",
   primaryDark: "#aa8929",
   secondary: "#7645D9",
